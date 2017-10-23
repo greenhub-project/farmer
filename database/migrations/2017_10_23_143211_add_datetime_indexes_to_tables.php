a883a5610de4273ad1d367e1b567ae784d515f62
Hugo Matalonga <hugo@hmatalonga.com>
2017-10-23 15:45:20 +0100

    Add migration to add indexes to devices, samples and processes tables

---
+++ database/migrations/2017_10_23_143211_add_datetime_indexes_to_tables.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDatetimeIndexesToTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('devices', function (Blueprint $table) {
+           $table->index('created_at');
+        });
+        Schema::table('samples', function (Blueprint $table) {
+            $table->index('created_at');
+        });
+        Schema::table('app_processes', function (Blueprint $table) {
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('devices', function (Blueprint $table) {
+            $table->dropIndex('created_at');
+        });
+        Schema::table('samples', function (Blueprint $table) {
+            $table->dropIndex('created_at');
+        });
+        Schema::table('app_processes', function (Blueprint $table) {
+            $table->dropIndex('created_at');
+        });
+    }
+}
