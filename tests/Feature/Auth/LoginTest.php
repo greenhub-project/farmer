c5b4c33de6cd196b5609c4e1a16f2d2e9dda7c8d
soren5 <pedrofilipecarvalho@mac.com>
2018-10-12 19:06:42 +0100

    feat: finish authentication testing
    add password recovery test
    add invalid password test
    add login cookies test

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -3,11 +3,14 @@
 namespace Tests\Feature\Auth;
 
 use App\Farmer\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
-
 
 class LoginTest extends TestCase
 {
+
+    use RefreshDatabase;
 
     public function test_user_can_view_a_login_form()
     {
@@ -32,12 +35,50 @@ class LoginTest extends TestCase
             'email' => $user->email,
             'password' => $password,
         ]);
-        $response->assertRedirect('/home');
+        $response->assertRedirect('/dashboard');
         $this->assertAuthenticatedAs($user);
     }
 
-    public function testExample()
+    public function test_user_cannot_login_with_incorrect_password()
     {
-        $this->assertTrue(true);
+        $user = factory(User::class)->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_remember_me_functionality()
+    {
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+
+        $response->assertRedirect('/dashboard');
+
+        $response->assertCookie(Auth::guard()->getRecallerName(), vsprintf('%s|%s|%s', [
+            $user->id,
+            $user->getRememberToken(),
+            $user->password,
+        ]));
+
+        $this->assertAuthenticatedAs($user);
+
     }
 }
