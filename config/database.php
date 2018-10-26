a51290cf4c25e7685a0da5ee17829086d615855b
Hugo Matalonga <hmatalonga@gmail.com>
2018-10-26 15:53:48 +0100

    chore: move testing configuration to .env.testing

--- config/database.php
+++ config/database.php
@@ -31,21 +31,6 @@ return [
     */
 
     'connections' => [
-        'mysql_testing' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('TEST_DB_DATABASE', 'test'),
-            'username' => env('TEST_DB_USERNAME', 'forge'),
-            'password' => env('TEST_DB_PASSWORD', 'forge'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
