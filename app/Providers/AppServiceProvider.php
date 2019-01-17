a608476fb42f450e7bccb293f42417dc63310026
Hugo Matalonga <hmatalonga@gmail.com>
2019-01-17 14:31:16 +0000

    refactor: prepend redirectifinscure middleware only in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use App\Http\Middleware\RedirectIfInsecure;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (app()->environment() === 'production') {
+            $kernel->prependMiddleware(RedirectIfInsecure::class);
+        }
+
         Blade::if('admin', function () {
             return Auth::user()->hasRole('admin');
         });
