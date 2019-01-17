492e60981a8832186844887b215f92958bdba55e
Hugo Matalonga <hmatalonga@gmail.com>
2019-01-17 14:11:08 +0000

    feat: add redirect middleware for cloudflare

---
+++ app/Http/Middleware/RedirectIfInsecure.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CloudflareProxies
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->setTrustedProxies([$request->getClientIp()]);
+        if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
