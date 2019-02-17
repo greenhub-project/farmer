ccdf179bbc1a48566e963fb5c262d7cec8823437
Hugo Matalonga <hmatalonga@gmail.com>
2019-02-17 18:18:48 +0000

    feat: add spinner component

---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="spinner bg-white mx-auto relative">
+    <div class="double-bounce1 w-full h-full absolute pin-t pin-l bg-blue-dark rounded-full"></div>
+    <div class="double-bounce2 w-full h-full absolute pin-t pin-l bg-blue-dark rounded-full"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spinner"
+};
+</script>
+
+<style scoped>
+.spinner {
+  width: 40px;
+  height: 40px;
+}
+
+.double-bounce1,
+.double-bounce2 {
+  opacity: 0.6;
+
+  -webkit-animation: sk-bounce 2s infinite ease-in-out;
+  animation: sk-bounce 2s infinite ease-in-out;
+}
+
+.double-bounce2 {
+  -webkit-animation-delay: -1s;
+  animation-delay: -1s;
+}
+
+@-webkit-keyframes sk-bounce {
+  0%,
+  100% {
+    -webkit-transform: scale(0);
+  }
+  50% {
+    -webkit-transform: scale(1);
+  }
+}
+
+@keyframes sk-bounce {
+  0%,
+  100% {
+    transform: scale(0);
+    -webkit-transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+    -webkit-transform: scale(1);
+  }
+}
+</style>
--- resources/js/components/UsersTable.vue
+++ resources/js/components/UsersTable.vue
@@ -11,58 +11,61 @@
         <div class="w-24">&nbsp;</div>
       </div>
     </div>
-    <div class="bg-white rounded-b-lg">
-      <div
-        class="flex items-center text-blue-darker text-sm p-4 table-item"
-        v-for="user of users"
-        :key="user.name"
-      >
-        <div class="w-12">
-          <img
-            class="inline-block h-8 w-8 rounded-full"
-            src="https://randomuser.me/portraits/lego/1.jpg"
-            :alt="user.name"
-          >
-        </div>
-        <div class="w-1/5 flex flex-col">
-          <span class="mb-1">{{ user.name }}</span>
-          <span class="text-grey-dark">#{{ user.id }}</span>
-        </div>
-        <div class="w-1/4">{{ user.email }}</div>
-        <div class="flex-1 capitalize">{{ user.roles[0].name }}</div>
-        <div class="flex-1">{{ user.created_at }}</div>
-        <div class="flex-1" title="Verified" v-if="user.email_verified_at !== null">
-          <svg
-            class="h-8 h-8 text-green"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path
-              class="heroicon-ui"
-              d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.54-4.46a1 1 0 0 1 1.42-1.42 3 3 0 0 0 4.24 0 1 1 0 0 1 1.42 1.42 5 5 0 0 1-7.08 0zM9 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm6 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
-            ></path>
-          </svg>
-        </div>
-        <div class="flex-1" title="Unverified" v-else>
-          <svg
-            class="h-8 h-8 text-red"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path
-              class="heroicon-ui"
-              d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.54-4.54a5 5 0 0 1 7.08 0 1 1 0 0 1-1.42 1.42 3 3 0 0 0-4.24 0 1 1 0 0 1-1.42-1.42zM9 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm6 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
-            ></path>
-          </svg>
-        </div>
-        <div class="w-24 text-right">
-          <span class="hover:underline hover:cursor-pointer mr-8">Toggle Role</span>
+    <div class="bg-white rounded-b-lg" :class="busy ? 'py-4' : ''">
+      <Spinner v-if="busy"/>
+      <div class="table-body" v-else>
+        <div
+          class="flex items-center text-blue-darker text-sm p-4 table-item"
+          v-for="user of users"
+          :key="user.name"
+        >
+          <div class="w-12">
+            <img
+              class="inline-block h-8 w-8 rounded-full"
+              src="https://randomuser.me/portraits/lego/1.jpg"
+              :alt="user.name"
+            >
+          </div>
+          <div class="w-1/5 flex flex-col">
+            <span class="mb-1">{{ user.name }}</span>
+            <span class="text-grey-dark">#{{ user.id }}</span>
+          </div>
+          <div class="w-1/4">{{ user.email }}</div>
+          <div class="flex-1 capitalize">{{ user.roles[0].name }}</div>
+          <div class="flex-1">{{ user.created_at }}</div>
+          <div class="flex-1" title="Verified" v-if="user.email_verified_at !== null">
+            <svg
+              class="h-8 h-8 text-green"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path
+                class="heroicon-ui"
+                d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.54-4.46a1 1 0 0 1 1.42-1.42 3 3 0 0 0 4.24 0 1 1 0 0 1 1.42 1.42 5 5 0 0 1-7.08 0zM9 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm6 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+              ></path>
+            </svg>
+          </div>
+          <div class="flex-1" title="Unverified" v-else>
+            <svg
+              class="h-8 h-8 text-red"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path
+                class="heroicon-ui"
+                d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.54-4.54a5 5 0 0 1 7.08 0 1 1 0 0 1-1.42 1.42 3 3 0 0 0-4.24 0 1 1 0 0 1-1.42-1.42zM9 11a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm6 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+              ></path>
+            </svg>
+          </div>
+          <div class="w-24 text-right">
+            <span class="hover:underline hover:cursor-pointer mr-8">Toggle Role</span>
+          </div>
         </div>
       </div>
     </div>
@@ -70,17 +73,24 @@
 </template>
 
 <script>
+import Spinner from "./Spinner";
 import UserService from "../services/UserService";
 
 export default {
   name: "UsersTable",
+  components: { Spinner },
   data() {
     return {
-      users: []
+      users: [],
+      busy: false
     };
   },
   created() {
-    UserService.index().then(({ data }) => (this.users = data));
+    this.busy = true;
+    UserService.index().then(({ data }) => {
+      this.users = data;
+      this.busy = false;
+    });
   }
 };
 </script>
