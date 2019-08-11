1368400bf101ddfcc61dd3ff9951b125b2463255
Hugo Matalonga <hmatalonga@gmail.com>
2019-08-11 20:42:53 +0100

    chore: update dataset files

--- resources/views/dashboard/datasets.blade.php
+++ resources/views/dashboard/datasets.blade.php
@@ -6,19 +6,19 @@
   <div class="flex flex-wrap md:-mx-2">
       <dataset-card 
           title="Everything" 
-          link="{{ url('/storage/dataset.zip') }}"
+          link="{{ url('/storage/dataset.7z') }}"
            body="Includes devices, samples and apps.">
       </dataset-card>
       
       <dataset-card 
           title="Samples" 
-          link="{{ url('/storage/samples.zip') }}"
+          link="{{ url('/storage/samples.7z') }}"
            body="Includes sample related files.">
       </dataset-card>
 
       <dataset-card 
           title="Devices" 
-          link="{{ url('/storage/devices.zip') }}"
+          link="{{ url('/storage/devices.7z') }}"
            body="Includes only devices file.">
       </dataset-card>
   </div>
